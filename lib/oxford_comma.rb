6ba379207583b8af2fa99dfa2729393936bb3426
Luke Sherwood <luke.sherwood55@gmail.com>
2019-12-23 15:10:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ if array.length == 1
 elsif array.length == 2
   array.join(" and ")
 else array.length > 2
-  array.map!.with_index(-1){|n| "and #{n}"}
-  array.join(", ")
+  new_array = array.map.with_index(-1){|n| "and #{n}"}
+  new_array.join(", ")
 end
 end
