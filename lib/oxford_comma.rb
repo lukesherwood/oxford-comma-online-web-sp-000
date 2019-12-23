36008cea20fc73b83f80c187eeaf8a2445ef3fce
Luke Sherwood <luke.sherwood55@gmail.com>
2019-12-23 15:04:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ if array.length == 1
 elsif array.length == 2
   array.join(" and ")
 else array.length > 2
-  array.map_with_index {|last, -1| "and #{last}"}
+  array.map_with_index {|last, 1| "and #{last}"}
   array.join(", ")
 end
 end
