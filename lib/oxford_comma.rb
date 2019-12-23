d55fac3ea82ade48e3710834f1e0eb4786825f31
Luke Sherwood <luke.sherwood55@gmail.com>
2019-12-23 15:00:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,12 @@ if array.length == 1
 elsif array.length == 2
   array.join(" and ")
 else array.length > 2
-  (array.last).map{|last| "and #{last}"}
+  (array.last).map {|last| "and #{last}"}
   array.join(", ")
 end
 end
+
+sharks = ["Hammerhead", "Great White", "Tiger", "Whale"]
+options = sharks.map {|shark| "<option>#{shark}</option>"}
+output = options.join("\n")
+print output
