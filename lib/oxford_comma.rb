16a59fad5d34e7b8bdeacaa54f66ef2972319295
Luke Sherwood <luke.sherwood55@gmail.com>
2019-12-23 14:59:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ if array.length == 1
 elsif array.length == 2
   array.join(" and ")
 else array.length > 2
-  array.last.map{|last| "and #{last}"}
+  (array.last).map{|last| "and #{last}"}
   array.join(", ")
 end
 end
